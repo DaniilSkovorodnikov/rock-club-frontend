a2ed06472222ad88e9b0774b6cded905335eb901
DaniilSkovorodnikov <skovorodnikov.danil@mail.ru>
2024-12-01 20:00:04 +0500

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const Profile: React.FC = () => {
     // const navigate = useNavigate();
